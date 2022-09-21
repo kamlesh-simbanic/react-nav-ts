665f6b7b76d6b9733a98ae04af47baf6e735d99b
kamlesh-simbanic <kamlesh.simbanic2022@gmail.com>
2022-09-21 12:33:11 +0530

    Code Optimization in attributes

--- src/Component/NavLink.jsx
+++ src/Component/NavLink.jsx
@@ -3,22 +3,16 @@ import React from "react";
 const NavLink = (props) => {
   return (
     <>
-      {props.url != undefined ? (
-        <a
-          href={props.url}
-          className={`nav-link px-0 align-middle  text-${props.color} `}
-        >
-          <span class=" d-none d-sm-inline">{props.label}</span>{" "}
-        </a>
-      ) : (
-        <a
-          role={"button"}
-          onClick={props.onClick}
-          className={`nav-link px-0 align-middle  text-${props.color} `}
-        >
-          <span class=" d-none d-sm-inline">{props.label}</span>{" "}
-        </a>
-      )}
+      <a
+        role={"button"}
+        {...(props.url != undefined
+          ? { href: { ...props.url } }
+          : { onClick: { ...props.onClick } })}
+        onClick={props.onClick}
+        className={`nav-link px-0 align-middle  text-${props.color} `}
+      >
+        <span class=" d-none d-sm-inline">{props.label}</span>{" "}
+      </a>
     </>
   );
 };
